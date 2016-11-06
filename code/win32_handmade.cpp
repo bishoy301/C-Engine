40df2676b73f0bbbafcd7425f744ed1e3c8589de
bishoy301 <bishoyrock2@gmail.com>
2016-11-06 15:40:07 -0600

    Setting up entry into program

---
+++ code/win32_handmade.cpp
@@ -0,0 +1,17 @@
+/* ========================================================================
+   $File: $
+   $Date: $
+   $Revision: $
+   $Creator: Bishoy Boktor $
+   $Notice: (C) Copyright 2016. All Rights Reserved. $
+   ======================================================================== */
+
+#include <windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance,
+                     HINSTANCE hPrevInstance,
+                     LPSTR lpCmdLine,
+                     int nCmdShow)
+{
+    return 0;
+}
